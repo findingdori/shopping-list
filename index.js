d20ff462598b3069d1ba002496e147b9d3c493b5
Dora Gil <git@doragil.me>
2019-05-04 11:17:35 -0400

    Added add item feature

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
   $(this).closest('li').remove();
   //console.log($(this.closest('li')));
 });
+
+$('#js-shopping-list-form').on('submit', function(event) {
+  //disable form submission
+  event.preventDefault();
+  
+  //get value of text field and assign to variable
+  let newListItem = $('#shopping-list-entry').val();
+
+  //append HTML for item to main shopping-list class
+  $('.shopping-list').append(`<li>
+    <span class="shopping-item">${newListItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
+  
+  //clear text field for next entry
+  $('#shopping-list-entry').val('');
+})
